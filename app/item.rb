efb4fca103bed8a6206daa461ce53b35d8242550
Alexander Riccio <test35965+learn@gmail.com>
2019-06-21 15:44:16 -0400

    solve lab

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,13 @@
 class Item
   attr_accessor :name, :price
-
+  @@all = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
+  end
+
+  def self.find(item_name)
+    @@all.find{ |item| item.name == item_name }
   end
 end
